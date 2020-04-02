4ba19b16f3e017e09b1a0050b7573606360c0b0a
Vikas Sahu <tezikhan1@gmail.com>
2020-04-02 18:35:13 +0530

    Changed the UI and added some options

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import axios from "axios";
 
 
 const CardList = (props) => (
-  <div>
+  <div className="cardlist">
     {props.profiles.map(profile => <Card key={profile.id} {...profile} />)}
   </div>
 );
 
 class Card extends React.Component {
+
   render() {
     const profile = this.props;
+    let p;
+    if (profile.blog) {
+      p = <div className="linkdin"><a href={profile.blog} target="_blank"><i className="fa fa-linkedin" ></i></a></div>
+    }
+    else {
+      p = <div></div>
+    }
     return (
       <div className="github-profile">
         <img src={profile.avatar_url} ></img>
         <div className="info">
           <div className="name">{profile.name}</div>
           <div className="company">{profile.company}</div>
+          <div className="location">{profile.location}</div>
+          {p}
+          <div className="git-hub"><a href={profile.html_url} target="_blank"><i className="fa fa-github"></i></a></div>
         </div>
       </div>
     );
@@ -24,16 +35,34 @@ class Card extends React.Component {
 }
 
 class Form extends React.Component {
-  state = { userName: '' };
+  state = {
+    userName: '',
+    search_history: []
+  };
   handleSubmit = async (event) => {
     var resp;
     event.preventDefault();
+
+    let r = this.props.profiles.map(a => {
+      if (a.login === this.state.userName) {
+        return 'true';
+      }
+      else
+        return null
+    })
+    if (r.includes('true')) {
+      return;
+    }
     try {
       resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
       this.props.onSubmit(resp.data);
+      let x = [...this.state.search_history]
+      x.push(this.state.userName);
+      this.setState({ search_history: x })
       this.setState({ userName: '' });
     }
     catch (err) {
+      console.log(err)
       console.error("Error response:");
       alert('NOT FOUND')
       console.error(err.response.data);    // ***
@@ -43,19 +72,41 @@ class Form extends React.Component {
       console.log(resp);
     }
 
+  };
+  filterFunction = () => {
+    this.state.search_history.forEach(element => {
+      return (
+        <div>{element}</div>);
+    })
 
   };
   render() {
+    console.log('hello')
+    const r = this.state.search_history.map(element => {
+      return (
+        <div></div>);
+    })
+    console.log(r)
+
+    console.log(this.state.search_history)
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={this.state.userName}
-          onChange={event => this.setState({ userName: event.target.value })}
-          placeholder="GitHub username"
-          required
-        />
-        <button>Add card</button>
+      <form onSubmit={this.handleSubmit} >
+
+        <div className="form_div">
+          <input
+            className="abc"
+            type="text"
+            value={this.state.userName}
+            onChange={event => this.setState({ userName: event.target.value })}
+            placeholder="GitHub username"
+            required
+          // onKeyUp ={ (event) => if(event.target.value===null{
+
+          // })}
+          />
+
+          <button>Add card</button>
+        </div>
       </form>
     );
   }
@@ -64,7 +115,8 @@ class Form extends React.Component {
 
 class App extends React.Component {
   state = {
-    profiles: [],
+    profiles: []
+
   };
   addNewProfile = (profileData) => {
     this.setState(prevState => ({
@@ -73,9 +125,10 @@ class App extends React.Component {
   };
   render() {
     return (
-      <div>
-        <div className="header">{this.props.title}</div>
-        <Form onSubmit={this.addNewProfile} />
+      <div className="main-div">
+        <div className="header">{this.props.title}
+        </div>
+        <Form onSubmit={this.addNewProfile} profiles={this.state.profiles} />
         <CardList profiles={this.state.profiles} />
       </div>
     );
