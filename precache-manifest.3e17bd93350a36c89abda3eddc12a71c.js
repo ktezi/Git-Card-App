86123cde695c97ea3c9c9826a564fe38fd015a65
Vikas Sahu <tezikhan1@gmail.com>
2020-04-02 18:23:39 +0530

    Updates

---
+++ precache-manifest.3e17bd93350a36c89abda3eddc12a71c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a724b58d7c31085ad4d70c6bf9f79986",
+    "url": "/Git-Card-App/index.html"
+  },
+  {
+    "revision": "4e8990f604ea04361377",
+    "url": "/Git-Card-App/static/css/main.a14be2ec.chunk.css"
+  },
+  {
+    "revision": "7383dc87a63d0b1a0bd9",
+    "url": "/Git-Card-App/static/js/2.1b3d09af.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Git-Card-App/static/js/2.1b3d09af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8990f604ea04361377",
+    "url": "/Git-Card-App/static/js/main.1e456062.chunk.js"
+  },
+  {
+    "revision": "175a6976e326c23dde8d",
+    "url": "/Git-Card-App/static/js/runtime-main.87bd7047.js"
+  }
+]);
